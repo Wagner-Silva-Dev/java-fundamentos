7544b1289c5986fdefca21d4c75c4bf7f5c504c8
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-06-08 19:50:52 -0300

    feat(enumeracao): adiciona atributo e construtor ao enum Semaforo

--- src/fundamentos/classes/enumeracao/Semaforo.java
+++ src/fundamentos/classes/enumeracao/Semaforo.java
@@ -1,7 +1,17 @@
 package fundamentos.classes.enumeracao;
 
 public enum Semaforo {
-    VERDE,
-    AMARELO,
-    VERMELHO;
+    VERDE("Pode seguir"),
+    AMARELO("Reduza a velocidade"),
+    VERMELHO("PARE!!!");
+
+    private final String mensagem;
+
+    Semaforo(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
 }
