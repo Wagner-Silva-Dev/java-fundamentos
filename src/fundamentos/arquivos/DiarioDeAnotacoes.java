127980c53e8e7e21cbdc285426fa726712dd7b49
Wagner Silva <249521811+Wagner-Silva-Dev@users.noreply.github.com>
2026-07-18 01:05:24 -0300

    feat(arquivos): implementa diário de anotações com leitura e escrita em arquivo

---
+++ src/fundamentos/arquivos/DiarioDeAnotacoes.java
@@ -0,0 +1,51 @@
+package fundamentos.arquivos;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class DiarioDeAnotacoes {
+    public static void main(String[] args) {
+        File diario = new File("src\\fundamentos\\arquivos\\diario_anotacoes.txt");
+        boolean existsDiario = diario.exists();
+
+        if (!existsDiario) {
+            try {
+                boolean isDiarioCreate = diario.createNewFile();
+                if (!isDiarioCreate) {
+                    System.out.println("Erro ao criar o diário!");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("Diario de anotações");
+
+        try (FileReader fr = new FileReader(diario); BufferedReader br = new BufferedReader(fr)) {
+            String linha;
+            boolean temConteudo = false;
+            while ((linha = br.readLine()) != null) {
+                System.out.println(linha);
+                temConteudo = true;
+            }
+            System.out.println(temConteudo ? "Continue suas anotações" : "Digite suas anotações");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (FileWriter fw = new FileWriter(diario, true); BufferedWriter bw = new BufferedWriter(fw); Scanner scanner = new Scanner(System.in)) {
+            do {
+                String anotacao = scanner.nextLine();
+                if (anotacao.equalsIgnoreCase("parar anotação")) {
+                    System.out.println("Finalizando anotações");
+                    break;
+                }
+
+                bw.newLine();
+                bw.write(anotacao);
+            } while (true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
